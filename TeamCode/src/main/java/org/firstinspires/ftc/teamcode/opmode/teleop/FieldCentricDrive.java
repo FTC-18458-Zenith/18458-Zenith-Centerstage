7f241c5bb56d22cf8cb91578500d83ace2a7a9cf
Joel Yowell <joelyowell9@gmail.com>
2023-10-09 12:49:41 -0500

    Field Centric drive??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/FieldCentricDrive.java
@@ -4,11 +4,10 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @TeleOp
-public class AnalogDrive extends LinearOpMode {
+public class FieldCentricDrive extends LinearOpMode {
     private DcMotor leftFront, leftRear, rightRear, rightFront;
     IMU imu = hardwareMap.get(IMU.class, "imu");
 
@@ -19,6 +18,12 @@ public class AnalogDrive extends LinearOpMode {
         leftRear = (DcMotor) hardwareMap.get("leftRear");
         rightFront = (DcMotor) hardwareMap.get("rightFront");
         rightRear = (DcMotor) hardwareMap.get("rightRear");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+
+
 /*
 - is Up, + is Down for Y axis sticks
  */
@@ -30,18 +35,13 @@ public class AnalogDrive extends LinearOpMode {
             if (gamepad1.options) {
                 imu.resetYaw();
             }
-
         }
     }
     public void moveDrive(double driveTrainPower) {
 
-        IMU imu = hardwareMap.get(IMU.class, "imu");
         double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
         double rotX = gamepad1.left_stick_x * Math.cos(-botHeading) - gamepad1.left_stick_y * Math.sin(-botHeading);
         double rotY = gamepad1.left_stick_x * Math.cos(-botHeading) + gamepad1.left_stick_y * Math.sin(-botHeading);
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
         rotX = rotX * 1.1;
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(gamepad1.right_stick_x), 1);
 
