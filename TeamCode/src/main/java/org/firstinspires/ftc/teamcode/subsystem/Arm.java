131cd7135681304fd5c7f6a3c02be945a45eb806
Joel Yowell <joelyowell9@gmail.com>
2023-10-20 13:39:35 -0500

    Added high, mid, low, etc positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
@@ -7,6 +7,16 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Arm {
     private final Servo leftArmServo, rightArmServo;
     private final Gamepad gamepad2;
+    private static final double HIGH = 1;
+    private static final double MID = 0.5;
+    private static final double LOW = 0.25;
+    private static final double INTAKE = 0;
+
+    //TODO:EDIT THESE VALUES TO CONSTRAINTS OF SERVO
+    private static double GEAR_RATIO = 0;
+    private static double READINGS_PER_REVOLUTION = 0;
+    private static double DEGREES_OF_FREEDOM = 0;
+    private static double GOAL_DEGREES = 0;
     public Arm(HardwareMap hardwareMap, Gamepad gamepad2) {
         leftArmServo = (Servo) hardwareMap.get("LeftArmServo");
         rightArmServo = (Servo) hardwareMap.get("RightArmServo");
@@ -14,15 +24,30 @@ public class Arm {
 
         leftArmServo.setPosition(0);
         rightArmServo.setPosition(0);
+        leftArmServo.setDirection(Servo.Direction.FORWARD);
+        rightArmServo.setDirection(Servo.Direction.REVERSE);
     }
     public void teleOp() {
-        //DEGREES = (GEAR_RATIO * READINGS_PER_REVOLUTION) / 360
-      if (gamepad2.a) armServos(0);
-      else if (gamepad2.b) armServos(0.5);
-      else if (gamepad2.y) armServos(0.75);
-      else if (gamepad2.x) armServos(1);
-    } public void armServos(double position) {
+        //DEGREES = (GEAR_RATIO * READINGS_PER_REVOLUTION) / (DEGREES_OF_FREEDOM) * DEGREES I WISH, DO NOT DO YET
+      if (gamepad2.a) moveIntake();
+      else if (gamepad2.b) moveLow();
+      else if (gamepad2.y) moveMid();
+      else if (gamepad2.x) moveHigh();
+    }
+    public void setPosition(double position) {
         leftArmServo.setPosition(position);
         rightArmServo.setPosition(position);
+    }
+    public void moveHigh() {
+        setPosition(HIGH);
+    }
+    public void moveMid() {
+        setPosition(MID);
+    }
+    public void moveLow() {
+        setPosition(LOW);
+    }
+    public void moveIntake() {
+        setPosition(INTAKE);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Slides.java
@@ -4,14 +4,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.sun.tools.javac.jvm.Gen;
 
 public class Slides {
     private final DcMotor leftSlide, rightSlide;
     private final Gamepad gamepad2;
-    private final int Gear_Ratio = 1;
-    private final int Gear_Diameter_Centimeters = 1;
-
+    final static double GEAR_RATIO = 1;
+    final static double TICKS_PER_REVOLUTION = 1150;
+    final static double GEAR_DIAMETER_CENTIMETERS = 3.2;
+    private static final double HIGH = 33.33;
+    private static final double MID = 25.75;
+    private static final double LOW = 19;
+    private static final double POSITION4 = 12.375;
+    private static final int INTAKE = 0;
+    final double CENTIMETER_TO_TICKS = (TICKS_PER_REVOLUTION * GEAR_RATIO) / (GEAR_DIAMETER_CENTIMETERS * Math.PI);
 
     public Slides(HardwareMap hardwareMap, Gamepad gamepad2) {
         leftSlide = (DcMotor) hardwareMap.get("leftSlide");
@@ -36,14 +41,35 @@ public class Slides {
     }
     public void teleOp() {
             //Depends on number of ticks per revolution
-        if (gamepad2.a) slideMotors(((1000 * Gear_Ratio) / (Gear_Diameter_Centimeters * Math.PI)));
-        else if (gamepad2.b)slideMotors(500);
-        else if (gamepad2.y) slideMotors(250);
-        else if (gamepad2.x) slideMotors(0);
+        if (gamepad2.dpad_up) moveHigh();
+        else if (gamepad2.b) moveMid();
+        else if (gamepad2.y) moveLow();
+        else if (gamepad2.x) moveToPOS4();
+        else if (gamepad2.a) moveToIntakeLevel();
     }
-    public void slideMotors(double targetPosition) {
-        //Centimeters = (GEAR_RATIO * TICKS_PER_REVOLUTION) / (GEAR_DIAMETER_CENTIMETERS * Math.PI);
-        leftSlide.setTargetPosition((int) targetPosition);
-        rightSlide.setTargetPosition((int) targetPosition);
+    public void moveSlides(double centimeters) {
+        //Centimeters = (GEAR_RATIO * TICKS_PER_REVOLUTION) / (GEAR_DIAMETER_CENTIMETERS * Math.PI)
+        leftSlide.setTargetPosition((int) (centimeters * CENTIMETER_TO_TICKS));
+        rightSlide.setTargetPosition((int) (centimeters * CENTIMETER_TO_TICKS));
+    }
+    public void moveHigh() {
+        leftSlide.setTargetPosition((int) HIGH);
+        rightSlide.setTargetPosition((int) HIGH);
+    }
+    public void moveMid() {
+        leftSlide.setTargetPosition((int) MID);
+        rightSlide.setTargetPosition((int) MID);
+    }
+    public void moveLow() {
+        leftSlide.setTargetPosition((int) LOW);
+        rightSlide.setTargetPosition((int) LOW);
+    }
+    public void moveToPOS4() {
+        leftSlide.setTargetPosition((int) POSITION4);
+        rightSlide.setTargetPosition((int) POSITION4);
+    }
+    public void moveToIntakeLevel() {
+        leftSlide.setTargetPosition(INTAKE);
+        rightSlide.setTargetPosition(INTAKE);
     }
 }
