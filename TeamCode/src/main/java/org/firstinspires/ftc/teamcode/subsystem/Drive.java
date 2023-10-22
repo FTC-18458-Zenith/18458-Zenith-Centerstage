ef988528b91939b0def96ee1964462113d3eaabd
Joel Yowell <joelyowell9@gmail.com>
2023-10-22 17:01:40 -0500

    idfk anymore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Drive.java
@@ -324,15 +324,16 @@ public class Drive extends MecanumDrive {
         }
     }
     public void moveDrive(double driveTrainPower) {
-        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-        double rotX = gamepad1.left_stick_x * Math.cos(-botHeading) - gamepad1.left_stick_y * Math.sin(-botHeading);
-        double rotY = gamepad1.left_stick_x * Math.cos(-botHeading) + gamepad1.left_stick_y * Math.cos(-botHeading);
-        //Try with sin with later if it still doesn't work
-        rotX = rotX * 1.1;
-        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(gamepad1.right_stick_x), 1);
         final double yAxisMovement = -gamepad1.left_stick_y;
         final double xAxisMovement = gamepad1.left_stick_x;
         final double rotationalMovement = gamepad1.right_stick_x;
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double rotX = gamepad1.left_stick_x * Math.cos(-botHeading) - gamepad1.left_stick_y * Math.sin(-botHeading);
+        double rotY = gamepad1.left_stick_x * Math.sin(-botHeading) + gamepad1.left_stick_y * Math.cos(-botHeading);
+        //Try with sin with later if it still doesn't work
+        rotX = rotX * 1.1;
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rotationalMovement), 1);
 
         leftFront.setPower(((yAxisMovement + xAxisMovement + rotationalMovement) * driveTrainPower)/denominator);
         leftRear.setPower(((yAxisMovement - xAxisMovement + rotationalMovement) * driveTrainPower)/denominator);
