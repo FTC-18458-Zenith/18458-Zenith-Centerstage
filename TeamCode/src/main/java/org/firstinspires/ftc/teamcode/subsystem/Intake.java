b26bad5d97c24e722dccfbb4f5612be389809a29
Joel Yowell <joelyowell9@gmail.com>
2023-11-12 15:03:39 -0600

    Stuff moving too fast?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -20,8 +20,8 @@ public class Intake {
         spinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     public void teleOp() {
-        if (gamepad2.right_bumper) spinnerIntakeThing(0.05);
-        else if (gamepad2.left_bumper) spinnerIntakeThing(-0.05);
+        if (gamepad2.right_bumper) spinnerIntakeThing(0.0000000000005);
+        else if (gamepad2.left_bumper) spinnerIntakeThing(-0.0000000000005);
         else spinnerIntakeThing(0);
     }
     public void soloTeleOp() {
