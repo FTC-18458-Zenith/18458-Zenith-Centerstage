9205bd4579daa0438d1b6e935e0c52013daa9946
Joel Yowell <joelyowell9@gmail.com>
2023-11-15 14:02:06 -0600

    meep meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,30 +2,54 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(900);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity redAlliance = new DefaultBotBuilder(meepMeep)
+                .setColorScheme((new ColorSchemeRedDark()))
+                .setDimensions(13.4,14.8031)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13.7)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 2)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(14, -60, Math.toRadians(90)))
-                                .forward(30)
+                                .forward(29)
                                 .turn(Math.toRadians(-90))
                                 .forward(30)
                                 .forward(-102)
                                 .strafeLeft(5)
                                 .build()
                 );
+        RoadRunnerBotEntity blueAlliance = new DefaultBotBuilder(meepMeep)
+                .setColorScheme((new ColorSchemeBlueDark()))
+                .setDimensions(13.4,14.8031)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-34, 68, Math.toRadians(180)))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .build()
+                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_OFFICIAL)
                 .setDarkMode(false)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(redAlliance)
+                .addEntity(blueAlliance)
                 .start();
     }
 }
