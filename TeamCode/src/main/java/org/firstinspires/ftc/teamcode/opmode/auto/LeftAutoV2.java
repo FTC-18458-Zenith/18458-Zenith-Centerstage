38f6fb05e15b7eadf4c20282f276ee956ca79b09
collectingtaxes <cheeseit313@gmail.com>
2023-11-16 14:31:37 -0600

    Autos -_-

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/LeftAutoV2.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.opmode.testing.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystem.Drive;
+import org.firstinspires.ftc.teamcode.util.trajectorysequence.TrajectorySequence;
+
+@Config
+@Autonomous (name = "LeftAutoV2", group = "RoadRunnerPath")
+
+public class LeftAutoV2 extends LinearOpMode {
+
+
+    public static double Park_Distance = 30;
+    public static double Pixel_Dis = 20;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        Drive drive = new Drive(this);
+
+        Pose2d startPos = new Pose2d(0, 0, Math.toRadians(0));
+        ElapsedTime timer = new ElapsedTime();
+
+        drive.setPoseEstimate(startPos);
+
+        TrajectorySequence Trajectory1 = drive.trajectorySequenceBuilder(startPos)
+                .forward(Pixel_Dis)
+                .build();
+
+        TrajectorySequence Trajectory2 = drive.trajectorySequenceBuilder(Trajectory1.end())
+                .turn(Math.toRadians(90))
+                        .back(Park_Distance)
+                                .build();
+
+        //Run Auto
+        drive.followTrajectorySequence(Trajectory1);
+
+        drive.followTrajectorySequence(Trajectory2);
+    }
+}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/LockToTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/testing/LockToTest.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode.opmode.testing;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.util.Angle;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.subsystem.Drive;
 
+@Autonomous
 public class LockToTest extends LinearOpMode {
-    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
     double xyP = 1;
     double headingP = 1;
 
@@ -15,12 +18,21 @@ public class LockToTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
     waitForStart();
     while (opModeIsActive()) {
-        lockTo(new Pose2d(0, 0, Math.toRadians(0)));
+
+        Drive drive = new Drive(this);
+
+        Pose2d startPos = new Pose2d(0, 0, Math.toRadians(0));
+
+        drive.setPoseEstimate(startPos);
+
+        lockTo(new Pose2d(5, 5, Math.toRadians(0)));
         drive.update();
     }
     }
 
     public void lockTo (Pose2d targetPos) {
+        Drive drive = new Drive(this);
+
         Pose2d currentPos = drive.getPoseEstimate();
         Pose2d difference = targetPos.minus(currentPos);
         Vector2d xy = difference.vec().rotated(-currentPos.getHeading());
