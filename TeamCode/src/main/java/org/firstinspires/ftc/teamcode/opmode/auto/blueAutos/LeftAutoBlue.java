f647e2aab77aa1fbebcddab5b45a0d1252ee8f17
Joel Yowell <joelyowell9@gmail.com>
2024-01-06 13:31:47 -0600

    Please tell me if I am cooking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/blueAutos/LeftAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/blueAutos/LeftAutoBlue.java
@@ -1,17 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmode.auto.blueAutos;
 
+import android.graphics.Camera;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.opmode.testing.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystem.Arm;
 import org.firstinspires.ftc.teamcode.subsystem.Drive;
 import org.firstinspires.ftc.teamcode.subsystem.Slides;
+import org.firstinspires.ftc.teamcode.subsystem.Vision;
+import org.firstinspires.ftc.teamcode.subsystem.pipelines.ColorDetection;
 import org.firstinspires.ftc.teamcode.util.trajectorysequence.TrajectorySequence;
 
 @Autonomous (name = "RightAutoBlue", group = "RoadRunnerPath")
 public class LeftAutoBlue extends LinearOpMode {
+    ColorDetection colorDetection = new ColorDetection();
     public static double Park_Distance = 80;
     public static double Spike_Dis = 29;
 
@@ -22,6 +28,8 @@ public class LeftAutoBlue extends LinearOpMode {
         Drive drive = new Drive(this);
         Arm arm = new Arm(this);
         Slides slides = new Slides(this);
+        ColorDetection colorDetection = new ColorDetection();
+        Vision vision = new Vision(this);
 
         Pose2d startPos = new Pose2d(-34, 68, Math.toRadians(90));
         ElapsedTime timer = new ElapsedTime();
@@ -37,6 +45,7 @@ public class LeftAutoBlue extends LinearOpMode {
                 .forward(Park_Distance)
                 .build();
         //Run Auto
+
         drive.followTrajectorySequence(Trajectory1);
         drive.followTrajectorySequence(Trajectory2);
         Thread.sleep(500);
@@ -44,5 +53,11 @@ public class LeftAutoBlue extends LinearOpMode {
         Thread.sleep(1000);
         Thread.sleep(1500);
         slides.reset();
+        switch (colorDetection.getLocation()) {
+            case "1":
+                drive.followTrajectorySequence(Trajectory1);
+            case "2":
+                 drive.followTrajectorySequence(Trajectory2);
+        }
     }
 }
