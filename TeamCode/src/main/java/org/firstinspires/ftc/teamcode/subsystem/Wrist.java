c8966e09c870c373552b6829e3956300882ff6d8
collectingtaxes <cheeseit313@gmail.com>
2024-01-11 19:48:16 -0600

    FTC Dashboard stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Wrist.java
@@ -5,12 +5,14 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Config
 public class Wrist extends SubsystemBase {
+
+    // testing to see if you can change booleans
+    public static boolean REVERSED = false;
 
     public final static double grabPose = 0,
             scorePose = 0,
@@ -22,7 +24,7 @@ public class Wrist extends SubsystemBase {
     public Wrist(final HardwareMap hMap, Telemetry telemetry) {
         this.Wrist = new SimpleServo(hMap, "Wrist", 0, 360);
 
-        Wrist.setInverted(false);
+        Wrist.setInverted(REVERSED);
 
         Wrist.setPosition(resetPose);
 
