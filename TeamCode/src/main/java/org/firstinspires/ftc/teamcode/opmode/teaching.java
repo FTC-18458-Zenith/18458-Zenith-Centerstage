b22cdd65c66801238a5651f9ef19489fcdd6872b
Joel Yowell <joelyowell9@gmail.com>
2024-02-28 13:01:38 -0600

    FieldCneticTurning done ( stole from apex lmao )

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teaching.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teaching.java
@@ -6,10 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class teaching extends LinearOpMode {
     DcMotor backRight, backLeft, frontRight, frontLeft;
+    Servo servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -19,6 +21,7 @@ public class teaching extends LinearOpMode {
             backRight.setPower(-gamepad1.left_stick_y);
             frontRight.setPower(-gamepad1.left_stick_y);
             frontLeft.setPower(-gamepad1.left_stick_y);
+            servo.setPosition(1);
         }
     }
 }
