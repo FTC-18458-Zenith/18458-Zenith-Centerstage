16b6e0913b66173b06d520a5c7f97a8296d5bf14
Joel Yowell <joelyowell9@gmail.com>
2024-03-26 16:56:10 -0500

    Sensors stuff that doesn't work yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/CommandBased/DistanceSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/CommandBased/DistanceSensor.java
@@ -5,18 +5,18 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class DistanceSensor extends SubsystemBase {
-        private final ColorSensor frontSensor; //backSensor;
+        private final ColorSensor frontSensor, backSensor;
         private final Telemetry telemetry;
 
         public DistanceSensor(Telemetry telemetry, HardwareMap hardwareMap) {
             this.frontSensor = hardwareMap.get(ColorSensor.class, "frontSensor");
-//            this.backSensor = hardwareMap.get(ColorSensor.class, "backSensor");
+            this.backSensor = hardwareMap.get(ColorSensor.class, "backSensor");
             this.telemetry = telemetry;
         }
 
         public void periodic() {
             telemetry.addData("Pixel in back: ", frontPixelDetection());
-//            telemetry.addData("Pixel in front: ", backPixelDetection());
+            telemetry.addData("Pixel in front: ", backPixelDetection());
         }
 
         public boolean whitePixelDetection(ColorSensor sensor) {
@@ -35,10 +35,10 @@ public class DistanceSensor extends SubsystemBase {
             return (sensor.blue() > 250 && sensor.red() > 1000 && sensor.green() > 1000);
         }
 
-//        public boolean frontPixelDetection() {
-//            return yellowPixelDetection(backSensor) || greenPixelDetection(backSensor) ||
-//                    purplePixelDetection(backSensor) || whitePixelDetection(backSensor);
-//        }
+        public boolean backPixelDetection() {
+            return yellowPixelDetection(backSensor) || greenPixelDetection(backSensor) ||
+                    purplePixelDetection(backSensor) || whitePixelDetection(backSensor);
+        }
 
         public boolean frontPixelDetection() {
             return yellowPixelDetection(frontSensor) || greenPixelDetection(frontSensor) ||
